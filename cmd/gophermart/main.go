be2dfcc2dafa41914b503a92d4582e38437109a1
desktop <dirol481@gmail.com>
2025-02-09 12:36:10 +0300

    Accrual collecting

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"github.com/labstack/echo/v4"
 	"github.com/llaxzi/gophermart/internal/apperrors"
 	"github.com/llaxzi/gophermart/internal/handler"
 	"github.com/llaxzi/gophermart/internal/middleware"
+	"github.com/llaxzi/gophermart/internal/orders"
 	"github.com/llaxzi/gophermart/internal/repository"
 	"github.com/llaxzi/gophermart/internal/tokens"
 	"github.com/llaxzi/retryables"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -19,8 +24,9 @@ func main() {
 	parseVars()
 
 	// Временно выставляем вручную
-	databaseDSN = "postgres://dev:qwerty@localhost:5433/gophermart?sslmode=disable"
+	/*databaseDSN = "postgres://dev:qwerty@localhost:5433/gophermart?sslmode=disable"
 	runAddr = ":8081"
+	accrualAddr = "http://localhost:8080"*/
 
 	repo, err := repository.NewRepository(databaseDSN)
 	if err != nil {
@@ -35,8 +41,8 @@ func main() {
 		return errors.Is(err, apperrors.ErrPgConnExc)
 	})
 
-	// Запуск миграции из приложения, при необходимости
-	/*err = repo.Bootstrap("postgres://dev:qwerty@localhost:5433/gophermart?sslmode=disable", 1)
+	// Запуск миграции из сервиса, при необходимости
+	/*err = repo.Bootstrap(databaseDSN, 1)
 	if err != nil {
 		log.Fatalf("Failed to bootstrap repository: %v", err)
 		return
@@ -50,17 +56,34 @@ func main() {
 
 	e := echo.New()
 
-	e.POST("/user/register", userHandler.Register)
-	e.POST("/user/login", userHandler.Login)
+	e.POST("/api/user/register", userHandler.Register)
+	e.POST("/api/user/login", userHandler.Login)
 
 	auth := e.Group("", mid.Auth)
 	gzip := auth.Group("", mid.Gzip)
 
-	auth.POST("/user/orders", userHandler.AddOrder)
-	gzip.GET("/user/orders", userHandler.GetOrders)
-	auth.GET("/user/balance", userHandler.GetBalance)
-	auth.POST("/user/balance/withdraw", userHandler.Withdraw)
-	gzip.GET("/user/balance/withdraw", userHandler.GetWithdrawals)
+	auth.POST("/api/user/orders", userHandler.AddOrder)
+	gzip.GET("/api/user/orders", userHandler.GetOrders)
+	auth.GET("/api/user/balance", userHandler.GetBalance)
+	auth.POST("/api/user/balance/withdraw", userHandler.Withdraw)
+	gzip.GET("/api/user/withdrawals", userHandler.GetWithdrawals)
 
-	e.Logger.Fatal(e.Start(runAddr))
+	processor := orders.NewProcessor(repo, retryer, accrualAddr, 1, 5)
+	ctx, cancel := context.WithCancel(context.Background())
+	go processor.ProcessOrders(ctx)
+
+	// Запускаем сервер
+	go func() {
+		if err = e.Start(runAddr); err != nil {
+			log.Printf("Shutting down server: %v", err)
+			cancel()
+		}
+	}()
+
+	// Перехватываем сигнал Ctrl+C
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, os.Interrupt, syscall.SIGTERM)
+
+	<-signalCh
+	cancel()
 }
